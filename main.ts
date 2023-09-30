60cb6a3a727f77a9c6eaf2a4739a176f6bd0b4dc
guengui25 <guengui25@hotmail.com>
2023-09-30 14:30:01 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 //======================================================================================
 // Space Seed
 //======================================================================================
+
+console.log("Space Seed \n")
+
 //1.------------------------------------------------------------------------------------
 
 type superviviente = {
@@ -67,13 +70,13 @@ const con_nombre = enterpise.pasajeros.filter((elem: superviviente ) => {
 
 //Se podría meter todo en la función (los console.log), pero considero más interesante aislar los datos y llamarlos por separado
 
-//console.log("Se sabe el nombre de ",con_nombre.length)
-
-//console.log(Object.values(con_nombre))
+console.log("Se sabe el nombre de ",con_nombre.length,"\n","Se conocen los siguientes datos: ",Object.values(con_nombre), " \n")
 
 //======================================================================================
 //The side of paradise
 //======================================================================================
+
+console.log("The side of paradise \n")
 
 //1.------------------------------------------------------------------------------------
 
@@ -108,32 +111,39 @@ const listar_sanos = pasajeros_afectados.forEach((elem: infeccion )=>{
     if(elem.infectado === false && elem.nombre !== undefined) console.log("Pasajero sano:",elem.nombre)
 })
 
-//listar_sanos; //Si hiciese console.log aquí daría undefined ya que forEach no devuelve nada
+listar_sanos; //Si hiciese console.log aquí daría undefined ya que forEach no devuelve nada
+
+console.log("\n");
 
 //2.------------------------------------------------------------------------------------
 
+//a)
 const algun_infectado = pasajeros_afectados.some((elem: infeccion)=> {
-    return elem.infectado === false 
+    return elem.infectado === true 
 })
 
-//console.log("Hay algún infectado en la nave:",algun_infectado);
+console.log("Hay algún infectado en la nave:",algun_infectado?"Si":"No","\n");
 
+//b)
 const todos_sanos_curados = pasajeros_afectados.every((elem:infeccion)=> {
     return elem.infectado === false
 }
 )
 
-//console.log("Están todos sanos:",todos_sanos_curados);
+console.log("Están todos sanos:",todos_sanos_curados?"Si":"No","\n");
 
+//c)
 const proximo_infectado = pasajeros_afectados.find((elem:infeccion)=> {
     return elem.infectado === true;
 })
 
-//console.log("El primer infectado de la lista de pasajeros es:",proximo_infectado);
+console.log("El primer infectado de la lista de pasajeros es:",proximo_infectado ||"No hay ninguno","\n");
 
 //======================================================================================
 //The city of the edge of forever
 //======================================================================================
+
+console.log("The city of the edge of forever \n")
 
 //1.------------------------------------------------------------------------------------
 
@@ -143,14 +153,14 @@ const anos = datos_incomprensibles.filter((elem:any)=> {
     return typeof elem === "number";
 })
 
-//console.log("Se han separado los anos:",anos);
+console.log("Se han separado los anos:",anos,"\n");
 
 //2.------------------------------------------------------------------------------------
 
-//Pese a que exista definida, como está limitado el tipo desde 1970 a la actualidad, y hay problemas para usarlo
+//Pese a que exista definida, como está limitado el tipo desde 1970 a la actualidad, lo cual no cumple con las normas de la Federación Unida de Planetas
 //https://linuxhint.com/convert-numbers-dates-javascript/
 
-//Prefiero crear mi propia fecha y asignarle valores aleatorios a los campos que faltan
+//Creo mi propio tipo fecha y le asigno valores aleatorios a los campos que faltan
 
 interface fecha {
     ano: number,
@@ -174,41 +184,108 @@ const fechas_completas = anos.map((elem: number) =>{
     return devolucion;
 })
 
-//console.log("Fechas completas",fechas_completas);
+console.log("Fechas completas:",fechas_completas,"\n");
 
 //======================================================================================
 //The trouble with Tribbles
 //======================================================================================
 
+console.log("The trouble with Tribbles \n")
+
 //Daños y nº de tribles
 
-const turboConductos: number[][] = [
+
+//Forma array de arrays
+const ListaTurboConductos: number[][] = [
     [15,10],
     [25,30],
     [5,2],
     [40,31]
 ]
 
-//1.
+//1.------------------------------------------------------------------------------------
 
-const total_tribbles_mas20 = turboConductos.reduce((acc,elem) => {
-    if(elem[0] > 20) return acc + elem[1]; //Si cumple la condición sumo el valor de los tribbles al acumulador
-    else return acc; //Si no lo cumple, lo retorno sin modificarlo
+const total_tribbles_mas20 = ListaTurboConductos.reduce((acc,elem) => {
+    if(elem[0] > 20) return acc + elem[1];      //Si cumple la condición sumo el valor de los tribbles al acumulador
+    else return acc;                            //Si no lo cumple, lo retorno sin modificarlo
 },0)
 
-console.log(total_tribbles_mas20);
+console.log("Total de tribles con mas de 20 de danos:",total_tribbles_mas20,"\n");
 
-//2.
+//2.------------------------------------------------------------------------------------
 
-const senal_lisa = turboConductos.flat();
+const senal_lisa = ListaTurboConductos.flat();
 
-//console.log(senal_lisa);
+console.log("Senal lisa:",senal_lisa,"\n");
 
 
-//3.
+//3.------------------------------------------------------------------------------------
 
-const senal_peligro = turboConductos.flatMap((elem: number[])=>{ 
-    if(elem[1] >= (elem[0]*0,5)) return elem
-}).filter(elem => typeof elem !== "undefined")
+const senal_peligro = ListaTurboConductos.flatMap(elem =>{ 
+    if(elem[1] >= (elem[0]*0,5)) return elem+",peligro"
+    else return elem+",no peligro"
+})
 
-//console.log(senal_peligro)
+//Se podría hacer solo con map normal, ya que al sustituir los datos de tipo number[] con string, no necesitas el flat, ya que no hay qué aplanar
+
+//He hecho que se devuelvan los datos junto al string, no comprendo si buscas solo "peligro" y "no peligro" o todo
+
+console.log("Senal de peligro:",senal_peligro,"\n")
+
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//Forma con Objeto
+
+interface turbo_conducto {
+    danos: number,
+    n_tribles: number
+}
+
+const lista_turbo_conductos: turbo_conducto [] = [
+    {
+    danos :15 , n_tribles:10
+    },
+    {
+    danos :25 , n_tribles:30
+    },
+    {
+    danos :5 , n_tribles:2
+    },
+    {
+    danos :40 , n_tribles:31
+    }
+]
+
+//1.------------------------------------------------------------------------------------
+
+const total_tribbles_mas20_2 = lista_turbo_conductos.reduce((acc,elem) => {
+    if(elem.danos > 20) return acc + elem.n_tribles;      //Si cumple la condición sumo el valor de los tribbles al acumulador
+    else return acc;                                      //Si no lo cumple, lo retorno sin modificarlo
+},0)
+
+console.log("(2) Total de tribles con mas de 20 de danos:",total_tribbles_mas20_2,"\n");
+
+//2.------------------------------------------------------------------------------------
+
+
+
+const senal_lisa_2 = lista_turbo_conductos.map(e=>Object.values(e)).flat(); 
+
+//Realmente al hacerlo con objeto y hacer map y luego flat sería mejor hacer flatmap(e=>Object.values(e)) pese a que sea literalmente lo mismo
+
+console.log("(2) Senal lisa:",senal_lisa_2,"\n");
+
+
+//3.------------------------------------------------------------------------------------
+
+const senal_peligro_2 = lista_turbo_conductos.flatMap(elem =>{ 
+    if(elem.n_tribles >= (elem.danos*0,5)) return Object.values(elem)+",peligro";
+    else return Object.values(elem)+",no peligro";
+})
+
+//Se podría hacer solo con map normal, ya que al sustituir los datos de tipo turbo_conducto con string, no necesitas el flat
+
+//He hecho que se devuelvan los datos junto al string, no comprendo si buscas solo "peligro" y "no peligro" o todo
+
+console.log("(2) Senal de peligro:",senal_peligro_2,"\n");
